28c66a0597401a9f7c8a6bc0c463908a45ff5fe6
Piotr Kuśpit <elmasterlow@gmail.com>
2024-10-18 10:29:17 +0200

    fix: Copy result message as reference not new object

--- eventiq_asb/results.py
+++ eventiq_asb/results.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Union
 
@@ -5,24 +6,43 @@ from azure.servicebus import ServiceBusReceivedMessage
 
 
 @dataclass
-class BaseResult:
+class BaseResult(ABC):
     message: ServiceBusReceivedMessage
 
+    @abstractmethod
+    def dict(self) -> dict:
+        ...
 
 @dataclass
 class Fail(BaseResult):
     reason: str
     action: str = "dead_letter_message"
 
+    def dict(self) -> dict:
+        return {
+            "message": self.message,
+            "action": self.action,
+            "reason": self.reason,
+        }
 
 @dataclass
 class Ack(BaseResult):
     action: str = "complete_message"
 
+    def dict(self) -> dict:
+        return {
+            "message": self.message,
+            "action": self.action,
+        }
 
 @dataclass
 class Nack(BaseResult):
     action: str = "abandon_message"
 
+    def dict(self) -> dict:
+        return {
+            "message": self.message,
+            "action": self.action,
+        }
 
 Result = Union[Ack, Nack, Fail]
